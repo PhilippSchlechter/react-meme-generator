ab717ab22549e807a30caa64f06bda5bf272e65d
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-16 17:56:03 +0200

    Add 3rd version

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React, { useState } from 'react';
 function App() {
   const [top, setTop] = useState('');
   const [bottom, setBottom] = useState('');
-  const [template, setTemplate] = useState('');
-  const memeUrl = `https://api.memegen.link/images/${template}/${top}/${bottom}.jpg`;
+  const [template, setTemplate] = useState('aag');
+  const memeUrl = `https://api.memegen.link/images/${template}/${top}/${bottom}`;
 
   return (
     <div>
       <h1>Meme Generator</h1>
+      <img src={memeUrl} alt="meme" data-test-id="meme-image" />
       <label>
+        <br />
         Top text
         <br />
         <input
@@ -36,7 +38,6 @@ function App() {
       </label>
       <br />
       <br />
-      <img src={memeUrl} alt="meme" data-test-id="meme-image" />
       <br />
 
       <label>
@@ -50,6 +51,17 @@ function App() {
           }}
         />
       </label>
+      <br />
+      <br />
+      <br />
+      <button
+        className="btn"
+        onClick={() => {
+          alert('A download would be nice');
+        }}
+      >
+        Download
+      </button>
     </div>
   );
 }
