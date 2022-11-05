3cb849aa5fc9ecfe183f81930e952d27623e638f
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-11-05 12:51:02 +0100

    Add download and select function, update eslint

--- src/App.js
+++ src/App.js
@@ -1,65 +1,120 @@
 import './App.css';
-import React, { useState } from 'react';
+import axios from 'axios';
+import fileDownload from 'js-file-download';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [top, setTop] = useState('');
-  const [bottom, setBottom] = useState('');
+  const [topMemeText, setTopMemeText] = useState('');
+  const [bottomMemeText, setBottomMemeText] = useState('');
   const [template, setTemplate] = useState('aag');
-  const memeUrl = `https://api.memegen.link/images/${template}/${top}/${bottom}`;
+  const [templates, setTemplates] = useState([]);
+  const memeUrl = topMemeText
+    ? `https://api.memegen.link/images/${template}/${topMemeText}/${bottomMemeText}.jpg`
+    : `https://api.memegen.link/images/${template}/_/${bottomMemeText}.jpg`;
+
+  const handleDownload = async () => {
+    await axios
+      .get(memeUrl, {
+        responseType: 'blob',
+      })
+      .then((res) => {
+        fileDownload(res.data, `meme_${template}.jpg`);
+      });
+  };
+  const handleClickBottom = () => {
+    // clear input value
+    setBottomMemeText('');
+  };
+  const handleClickTop = () => {
+    // clear input value
+    setTopMemeText('');
+  };
+
+  useEffect(() => {
+    fetch('https://api.memegen.link/templates')
+      .then((response) => response.json())
+      .then((response) => setTemplates(response))
+      .catch(() => alert('No template found.'));
+  }, []);
+
+  // First version, but then decided to implement the select function
+
+  /* const handleChange = (event) => {
+    setTemplate(event.target.value);
+  };*/
 
   return (
     <div>
       <h1>Meme Generator</h1>
-      <img src={memeUrl} alt="meme" data-test-id="meme-image" />
+
+      <img
+        className="imageContainer"
+        src={memeUrl}
+        alt="meme"
+        data-test-id="meme-image"
+        height="300px"
+        width="350px"
+      />
+
       <label>
         <br />
         Top text
         <br />
         <input
-          value={top}
+          value={topMemeText}
+          onClick={handleClickTop}
           onChange={(event) => {
-            setTop(event.currentTarget.value);
+            setTopMemeText(event.currentTarget.value);
           }}
         />
       </label>
-      <br />
-      <br />
-      <br />
-      <br />
-      <label>
-        Bottom text
-        <br />
-        <input
-          value={bottom}
-          onChange={(event) => {
-            setBottom(event.currentTarget.value);
-          }}
-        />
-      </label>
-      <br />
       <br />
       <br />
 
       <label>
-        <br />
-        Meme template
+        Bottom text
         <br />
         <input
-          value={template}
+          value={bottomMemeText}
+          onClick={handleClickBottom}
           onChange={(event) => {
-            setTemplate(event.currentTarget.value);
+            setBottomMemeText(event.currentTarget.value);
           }}
         />
       </label>
       <br />
+      <label>
+        <br />
+        Meme template
+        <br />
+        <select
+          name="templates"
+          id="templates"
+          className="templateSelector"
+          value={template}
+          onChange={(event) => setTemplate(event.currentTarget.value)}
+        >
+          {templates.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.id}
+            </option>
+          ))}
+        </select>
+        {/* <form>
+          <input
+            value={template}
+            onChange={handleChange}
+            onClick={handleClick}
+
+          />
+        </form> */}
+      </label>
+
       <br />
       <br />
-      <button
-        className="btn"
-        onClick={() => {
-          alert('A download would be nice');
-        }}
-      >
+
+      <br />
+      <button className="btn" onClick={handleDownload}>
         Download
       </button>
     </div>
